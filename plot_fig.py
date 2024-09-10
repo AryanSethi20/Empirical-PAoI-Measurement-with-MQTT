de266a7329228af286baed5f43f21943d29200b8
AryanSethi20 <aryansethi20@gmail.com>
2024-09-11 05:09:56 +0800

    Revert "first commit"
    This reverts commit e74fee37dd89b6f3b06c68c83eef4a9ac77d19d8.

--- plot_fig.py
+++
@@ -1,9 +0,0 @@
-from mqtt_client_sub import * # imports python script to call the plotting functions
-
-def main():    
-    # Plot results
-    # plot_PAoI_violation_probability_vs_thres()
-    plot_average_PAoI()
-# Runs main()
-if __name__ == "__main__":
-    main()
