246d4d06bca07bd66bca69875297898862a5a78e
AryanSethi20 <aryansethi20@gmail.com>
2024-09-11 05:13:32 +0800

    Reapply "first commit"
    This reverts commit de266a7329228af286baed5f43f21943d29200b8.

---
+++ plot_fig.py
@@ -0,0 +1,9 @@
+from mqtt_client_sub import * # imports python script to call the plotting functions
+
+def main():    
+    # Plot results
+    # plot_PAoI_violation_probability_vs_thres()
+    plot_average_PAoI()
+# Runs main()
+if __name__ == "__main__":
+    main()
